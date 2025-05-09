30aaeae0b55de6109e9280aefd13b3d35eca9e78
Rasmiya22-tech <arasmiya77@gmail.com>
2025-05-09 22:32:24 +0530

    add frontendport

--- HRWorkForceSystemBackend/HRWorkForceSystemBackend/Program.cs
+++ HRWorkForceSystemBackend/HRWorkForceSystemBackend/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3001") // React app URL
+            policy.WithOrigins("http://localhost:3000") // React app URL
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
